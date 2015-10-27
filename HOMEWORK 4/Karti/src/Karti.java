03416da9e033f43da7ff53c7da17afeb131cd09f
PakostiN <gravediggaz@abv.bg>
2015-10-27 15:03:31 +0200

    HOMEWORK 4

---
+++ HOMEWORK 4/Karti/src/Karti.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Karti {
+
+	private static Scanner test;
+
+	public static void main(String[] args) {
+		test = new Scanner (System.in);
+		System.out.println("Класическото тесте карти се състои от 13 различни вида карти съответно 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K and A. Направете програма, която приема от конзолата един символ и принтира 'Валидна карта', ако символът представлява валидна карта, и съответно принтира 'Невалидна карта', ако символът е на невалидна карта.");
+		System.out.println(" ");
+		System.out.println("2,3,4,5,6,7,8,9,10,J,Q,K,A");
+		System.out.println("Моля въведете символ : ");
+		String str  = test.next();
+		if (str.equals("2")) {
+			System.out.println("Картата е валидна!");
+		}
+		else if (str.equals("3")){
+			System.out.println("Картата е валидна!");
+		}
+		else if (str.equals("4")){
+			System.out.println("Картата е валидна!");
+		}
+		else if (str.equals("5")){
+			System.out.println("Картата е валидна!");
+		}
+		else if (str.equals("6")) {
+			System.out.println("Картата е валидна!");
+		}
+		else if (str.equals("7")) {
+			System.out.println("Картата е валидна!");
+		}
+		else if (str.equals("8")) {
+			System.out.println("Картата е валидна!");
+		}
+		else if (str.equals("9")){
+			System.out.println("Картата е валидна!");
+		}
+		else if (str.equals("10")){
+			System.out.println("Картата е валидна!");
+		}
+		else if (str.equals("J")) {
+			System.out.println("Картата е валидна!");
+		}
+		else if (str.equals("Q")) {
+			System.out.println("Картата е валидна!");
+		}
+		else if (str.equals("K")){
+			System.out.println("Картата е валидна!");
+		}
+		else if (str.equals("A")) {
+			System.out.println("Картата е валидна!");
+		}
+		else {
+			System.out.println("Картата е невалидна!");
+		}
+		test.close();
+
+		}
+		}
