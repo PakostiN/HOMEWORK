97180def69a5d9dd86ebe376e9cba795f7e35f21
PakostiN <gravediggaz@abv.bg>
2015-10-27 15:11:31 +0200

    HOMEWORK

--- HOMEWORK 4/BonusScores/src/BonusScores.java
+++ HOMEWORK 4/BonusScores/src/BonusScores.java
@@ -5,27 +5,25 @@ public class BonusScores {
 	private static Scanner test;
 
 	public static void main(String[] args) {
-		test = new Scanner (System.in, "UTF-8");
-		System.out.println("Напишете програма, която изчислява бонус точки на база резултат от игра. Потребителят трябва да въведе своите точки в интервалa[1;9]. Ако точките са в интервала [1;3], умножете ги по 5. Ако точките са в интервала [4;6], умножете ги по 10. Ако точките са в интервала [7;9], умножете ги по 50. Резултатът изведете на екрана.");
+		test = new Scanner(System.in, "UTF-8");
+		System.out.println(
+				"Напишете програма, която изчислява бонус точки на база резултат от игра. Потребителят трябва да въведе своите точки в интервалa[1;9]. Ако точките са в интервала [1;3], умножете ги по 5. Ако точките са в интервала [4;6], умножете ги по 10. Ако точките са в интервала [7;9], умножете ги по 50. Резултатът изведете на екрана.");
 		System.out.println(" ");
 		System.out.println("Моля въведете вашите точки в интервала от 0 до 9:");
 		int num = test.nextInt();
-		if (num >= 1 && num <= 3){
+		if (num >= 1 && num <= 3) {
 			System.out.print("Вашите точки + бонуса са : ");
-			System.out.println(num*5);
-		}
-		else if (num >=4 && num <=6) {
+			System.out.println(num * 5);
+		} else if (num >= 4 && num <= 6) {
 			System.out.print("Вашите точки + бонуса са : ");
-			System.out.println(num*10);
-		}
-		else if (num >= 7 && num <= 9) {
+			System.out.println(num * 10);
+		} else if (num >= 7 && num <= 9) {
 			System.out.print("Вашите точки + бонуса са : ");
-			System.out.println(num*50);
-		}
-		else {
+			System.out.println(num * 50);
+		} else {
 			System.out.println("Невалиден брой точки!!");
 		}
-	test.close();
+		test.close();
 	}
 
 }
